d37f2cac4598d009b88d6fae62865dda3f44fc7a
Paulo1302 <145205716+Paulo1302@users.noreply.github.com>
2024-10-16 17:47:34 -0300

    Update Usuario.java

--- vendaingressos/src/Usuario.java
+++ vendaingressos/src/Usuario.java
@@ -1,16 +1,3 @@
-/*******************************************************************************
-#Autor: Paulo Gabriel da Rocha Costa Silva
-#Componente Curricular: EXA 863 - MI - Programação
-#Concluido em: 16/09/2024
-
-
-#Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
-#trecho de código de outro colega ou de outro autor, tais como provindos de livros e
-#apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
-#de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
-#do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
-#******************************************************************************************/
-
 
 
 import java.util.List;
